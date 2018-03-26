c42415cb5f96b7c5fe1b97abd5d8f176c2a199cc
Charles <saecharles@gmail.com>
2018-03-25 20:01:49 -0700

    Code

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float x, y;
+
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        do { x = Random.Range(-2, 2) * .05f; }
+        while (x == 0);
+        do { y = Random.Range(-2, 2) * .05f; }
+        while (y == 0);
+    }
+    void Update() {
+        Vector2 ballPath = new Vector2(x, y);
+        rb.transform.Translate(ballPath);
+        GameObject[] blocks = GameObject.FindGameObjectsWithTag("block");
+        if (blocks.Length == 0) { SceneManager.LoadScene("main", LoadSceneMode.Single); }
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "isBorder") {
+            x = x * -1;
+
+        }
+        if (col.gameObject.tag == "isCieling" || col.gameObject.tag == "paddle")
+        {
+            y = y * -1;
+
+        }
+        if (col.gameObject.tag == "TopBot" )
+        {
+            y = y * -1;
+            Destroy(col.gameObject.transform.parent.gameObject);
+
+        }
+        if (col.gameObject.tag == "Side")
+        {
+            x = x * -1;
+            Destroy(col.gameObject.transform.parent.gameObject);
+
+        }
+        
+        if (col.gameObject.tag == "isDeath")
+        {           
+           SceneManager.LoadScene("main", LoadSceneMode.Single);     
+            
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/BrickGen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickGen : MonoBehaviour {
+    public GameObject yellow, red, purple;
+
+    const float ORIGIN = -6.5f;
+    int row = 8, column = 23;
+    float x = ORIGIN, y = 4;
+    GameObject[] bricks = {};
+
+    // Use this for initialization
+    void Start () {
+        GameObject[] temp = {yellow, red, purple};
+        bricks = temp;
+        for (int r = 0; r < row; r++) {
+            int count = 0;
+            for (int c = 0; c < column; c++) {
+                
+                if (count == 5) {
+                    c = c+1;
+                    x = x + 0.5f * 2;
+                    count = 0;                    
+                }
+                count++;                
+                Instantiate(bricks[Random.Range(0,3)], new Vector2(x, y), Quaternion.identity);
+                x  = x + .5f;
+            }
+            x = ORIGIN;
+            y = y-.4f;
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
